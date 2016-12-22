8a59a8e7802cc087077cecc0ef1d51335aac1456
immunarray <justin.pitts@immunarray.com>
2016-12-22 14:38:03 -0500

    adding intial permissions

---
+++ climb.hard/src/climb/hard/permisions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes import permissions
+
+AddRoute = "Climb Hard: Add Route"
+AddSite = "Climb Hard: Add Site"
+AddSetter = "Climb Hard: Add Setter"
+AddParty = "Climb Hard: Add Party Event"
